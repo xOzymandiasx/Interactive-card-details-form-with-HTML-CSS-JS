a76ba732603b073240aa5229e5363ca4b976ad29
Sergio <sergioottati@hotmail.com>
2022-09-12 15:26:15 -0300

    keyUp event finalizado

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-const $cardNumber = document.querySelector(".cardN");
-const $cardValue = document.querySelector("#cNumber");
 const $form = document.querySelector("form");
 $form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -10,14 +8,56 @@ document.addEventListener("keyup", (e) => {
     const $nameV = document.querySelector("#name");
     const $nameC = document.querySelector(".cName");
     $nameV.value === ""
-      ? $nameC.textContent = "Jane Appleseed"
-      : $nameC.textContent = $nameV.value;
+      ? ($nameC.textContent = "Jane Appleseed")
+      : ($nameC.textContent = $nameV.value);
   }
-});
 
-$cardValue.addEventListener("keyup", (e) => {
-  $cardNumber.textContent = $cardValue.value;
-  if ($cardNumber.textContent === "") {
-    $cardNumber.textContent = "0000 0000 0000 0000";
+  if (e.target.matches("#cNumber")) {
+    const $numberV = document.querySelector("#cNumber");
+    const $numberC = document.querySelector(".cardN");
+    $numberV.value === ""
+      ? ($numberC.textContent = "0000 0000 0000 0000")
+      : ($numberC.textContent = $numberV.value);
+  }
+
+  if (e.target.matches("#month")) {
+    const $monthV = document.querySelector("#month");
+    const $monthC = document.querySelector(".cDate");
+    if ($monthV.value === "") {
+      let month = $monthC.textContent.split("/");
+      month[0] = "00";
+      month = month.join("/");
+      $monthC.textContent = month;
+    } else {
+      let month = $monthC.textContent.split("/");
+      month[0] = $monthV.value;
+      month = month.join("/");
+      $monthC.textContent = month;
+    }
+  }
+
+  if (e.target.matches("#year")) {
+    const $yearV = document.querySelector("#year");
+    const $yearC = document.querySelector(".cDate");
+    if ($yearV.value === "") {
+      let month = $yearC.textContent.split("/");
+      month[1] = "00";
+      month = month.join("/");
+      $yearC.textContent = month;
+    } else {
+      let month = $yearC.textContent.split("/");
+      month[1] = $yearV.value;
+      month = month.join("/");
+      $yearC.textContent = month;
+    }
+  }
+
+  if (e.target.matches("#cvc")) {
+    console.log("hola");
+    const $cvcV = document.querySelector("#cvc");
+    const $cvcC = document.querySelector(".cvcBC");
+    $cvcV.value === ""
+      ? ($cvcC.textContent = "000")
+      : ($cvcC.textContent = $cvcV.value);
   }
 });
