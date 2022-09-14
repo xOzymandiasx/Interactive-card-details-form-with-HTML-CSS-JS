fa2a9e54b116854de8e22c4a731ccfd37cf959a9
Sergio <sergioottati@hotmail.com>
2022-09-14 14:35:50 -0300

    Finalizando validaciones

--- js/script.js
+++ js/script.js
@@ -1,14 +1,67 @@
 const $form = document.querySelector("form");
+const $nameV = document.querySelector("#name");
 const $numberV = document.querySelector("#cNumber");
+const $monthV = document.querySelector("#month");
+const $yearV = document.querySelector("#year");
+const $cvcV = document.querySelector("#cvc");
+
+const validateName = () => {
+  let mError = document.querySelectorAll(".error")[0];
+  if ($nameV.value === "") {
+    mError.textContent = "Cant be empty";
+  } else if ($nameV.value.match(/[^a-zA-Z\s]/g)) {
+    mError.textContent = "Cant be numbers";
+  } else {
+    mError.textContent = "";
+    return true;
+  }
+};
+
 const validateCNumber = () => {
   let mError = document.querySelectorAll(".error")[1];
-  let correctid = $numberV.value.split(" ")
+  let correctid = $numberV.value.split(" ");
   if ($numberV.value === "") {
     mError.textContent = "Cant be empty";
   } else if ($numberV.value.match(/[^0-9\s]/g)) {
     mError.textContent = "Wrong format, numbers only";
-  } else if ($numberV.value.length < 19 || correctid[0] < 4 || correctid[1] < 4 || correctid[2] < 4 || correctid[3] < 4) {
+  } else if (
+    $numberV.value.length < 19 ||
+    correctid[0] < 4 ||
+    correctid[1] < 4 ||
+    correctid[2] < 4 ||
+    correctid[3] < 4
+  ) {
     mError.textContent = "Invalid id card";
+  } else {
+    mError.textContent = "";
+    return true;
+  }
+};
+
+const validateDate = () => {
+  let mError = document.querySelectorAll(".error")[2];
+  if ($monthV.value === "" || $yearV.value === "") {
+    mError.textContent = "Cant be empty";
+  } else if (
+    $monthV.value < 12 ||
+    $monthV.value.length < 2 ||
+    $yearV.value < 2
+  ) {
+    mError.textContent = "Invalid date";
+  } else {
+    mError.textContent = "";
+    return true;
+  }
+};
+
+const validateCVC = () => {
+  let mError = document.querySelectorAll(".error")[3];
+  if ($cvcV.value === "") {
+    mError.textContent = "Cant be empty";
+  } else if ($cvcV.value.match(/[^0-9]/g)) {
+    mError.textContent = "Wrong format";
+  } else if ($cvcV.value.length < 3) {
+    mError.textContent = "Invalid cvc";
   } else {
     mError.textContent = "";
     return true;
@@ -18,6 +71,9 @@ const validateCNumber = () => {
 $form.addEventListener("submit", (e) => {
   e.preventDefault();
   validateCNumber();
+  validateName();
+  validateDate();
+  validateCVC();
 });
 
 document.addEventListener("keyup", (e) => {
